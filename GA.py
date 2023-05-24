c1b7099daa5c6eab69be5c57b3180a32f04387bd
Guilherme Peinador Gomes <guipeinador@hotmail.com>
2023-05-24 10:43:36 -0300

    GA + Propagador

--- GA.py
+++ GA.py
@@ -4,32 +4,6 @@ from comunicacao import calculacomunicacao
 import pygad
 from datetime import datetime
 
-'''def Fitness(x):
-
-    Rann = x[0]
-    Inclinacao = x[1]
-    Elevacao = x[2] + 6378
-
-    dt = 10
-
-    input_string = ' 11/10/2022 18:00:00'
-    data = datetime.strptime(input_string, " %m/%d/%Y %H:%M:%S")
-    df = propagador_orbital(data, Elevacao, 0.002, Raan, 0.0, 0.0, Inclinacao, NOrb, dt, 3.0, 0.1, 0.1, 0.2) #(data, semi_eixo, excentricidade, Raan, argumento_perigeu, anomalia_verdadeira,
-                                                # inclinacao, num_orbitas, delt, massa, largura, comprimento, altura)
-
-    from comunicacao import CalculaComuni
-    df2 = CalculaComuni(df)
-
-    df2 = df2[0:-1]
-    index = df2["Contato"].tolist()
-    Tempo = df2["Tempo"].tolist()
-
-    tempo_comunicacao_simulacao = index.count(1)
-    tempo_comunicacao_total = tempo_comunicacao_simulacao*dt
-    tempo_voo = len(index)*dt
-
-    return tempo_comunicacao_total/tempo_voo'''
-
 def fitness_func(solution, solution_idx):
     raan  = solution[0] #Raan
     inc  = solution[1]  #Inclinação
@@ -66,14 +40,14 @@ def on_generation(ga_instance):
     last_fitness = ga_instance.best_solution(pop_fitness=ga_instance.last_generation_fitness)[1]
 
 
-ga_instance = pygad.GA(num_generations=5,
-                       num_parents_mating=3,
+ga_instance = pygad.GA(num_generations=10,
+                       num_parents_mating=5,
                        fitness_func=fitness_func,
-                       sol_per_pop=5,
+                       sol_per_pop=10,
                        num_genes=3,
-                       gene_space=[{"low": 0, "high": 360} , [30, 50, 98] , {"low":6778, "high": 8000}],
-                       mutation_percent_genes= 70,
-                       mutation_by_replacement=True,
+                       gene_space=[{"low": 0, "high": 360} , {"low": 90, "high": 100} , {"low":6878, "high": 6978}], #[30, 50, 98]
+                       mutation_type="adaptive",
+                       mutation_probability= [0.5, 0.1],
                        on_generation=on_generation )
 
 ga_instance.run()
